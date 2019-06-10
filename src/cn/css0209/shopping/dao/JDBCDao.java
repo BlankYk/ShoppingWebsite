919b88b6d37071dbe51c22ed96b0303f1e4fba55
BlankYk <917885215@qq.com>
2019-06-10 10:32:35 +0800

    readme

--- src/cn/css0209/shopping/dao/JDBCDao.java
+++ src/cn/css0209/shopping/dao/JDBCDao.java
@@ -9,7 +9,7 @@ import cn.css0209.shopping.user.UserDo;
 import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
 
-public class JDBCDao{
+public class JDBCDao {
 
 	public boolean add(String name, String pwd) {
 		// TODO 自动生成的方法存根
@@ -20,15 +20,15 @@ public class JDBCDao{
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, name);
 			pstmt.setString(2, pwd);
-			if(pstmt.executeUpdate()>0) {
+			if (pstmt.executeUpdate() > 0) {
 				return true;
 			}
-			
+
 		} catch (SQLException e) {
 			// TODO 自动生成的 catch 块
 			e.printStackTrace();
 		}
-		
+
 		return false;
 	}
 
@@ -42,74 +42,83 @@ public class JDBCDao{
 			pstmt.setString(1, userDo.getName());
 			pstmt.setString(2, userDo.getPwd());
 			ResultSet rs = pstmt.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				return true;
-			}else {
+			} else {
 				return false;
 			}
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
 	}
-	
-	public boolean addCart(String username,String[] cart) {
+
+	public boolean addCart(String username, String[] cart) {
 		Connection conn = DbUtil.getConnection();
 		Log log = LogFactory.get(getClass());
 		String sql = "update user set cart = ? where username = ?";
 		try {
 			String carts = findCart(username);
-			for(String x:cart) {
-				if(carts.matches(".*"+x+"(.*)")) {
+			for (String x : cart) {
+				if (carts.matches(".*" + x + "(.*)")) {
 					return false;
 				}
 			}
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt = conn.prepareStatement(sql);
-			for(int i=1;i<=cart.length;i++) {
-				pstmt.setString(1, carts+="["+cart[i-1]+"]");
+			for (int i = 1; i <= cart.length; i++) {
+				pstmt.setString(1, carts += "[" + cart[i - 1] + "]");
 			}
 			pstmt.setString(2, username);
 			int flag = pstmt.executeUpdate();
 //			System.out.println(flag);
-			if(flag>=0) {
+			if (flag >= 0) {
 				return true;
 			}
-		}catch(NullPointerException e) {
+		} catch (NullPointerException e) {
 			Connection conn2 = DbUtil.getConnection();
 			Log log2 = LogFactory.get(getClass());
 			String sql2 = "update user set cart = ? where username = ?";
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, x);
-			pstmt.setString(2, username);
-			int flag = pstmt.executeUpdate();
+			PreparedStatement pstmt;
+			try {
+				pstmt = conn.prepareStatement(sql);
+
+				pstmt = conn.prepareStatement(sql);
+				for (int i = 0; i <= cart.length; i++) {
+					pstmt.setString(1, "[" + cart[i] + "]");
+				}
+				pstmt.setString(2, username);
+				int flag = pstmt.executeUpdate();
 //			System.out.println(flag);
-			if(flag>=0) {
-				return true;
+				if (flag >= 0) {
+					return true;
+				}
+			} catch (SQLException e1) {
+				// TODO 自动生成的 catch 块
+				e1.printStackTrace();
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return false;
 	}
-	
+
 	public String findCart(String username) {
 		Connection conn = DbUtil.getConnection();
-		
+
 		try {
 			String sql = "select cart from user where username = ?";
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, username);
 			ResultSet rs = pstmt.executeQuery();
-			if(rs.next() && rs != null) {
+			if (rs.next() && rs != null) {
 				return rs.getString("cart");
-			}else {
+			} else {
 				return "";
 			}
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 			return "";
 		}
